f6450c5e57fa9997e52a8faff0916467b0bcd4b3
Wilfred Erdo <wilfred05777@gmail.com>
2024-07-01 10:36:08 +0800

    practice sql

---
+++ 05-create-employers-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employers(
+    company_name varchar(250),
+    company_address varchar(300),
+    -- yearly_revenue FLOAT(5,2),
+    yearly_revenue NUMERIC(5,2),
+	is_hiring BOOLEAN -- High light sa kani na section ang boolean
+);
