73f9adf1410d36365112048a7dcf7ffa61f01d61
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-07-21 16:51:09 +0800

    63. Creating Generated Columns

--- 18-generated-column.sql
+++ 18-generated-column.sql
@@ -1,21 +1,21 @@
 -- https://github.com/academind/sql-complete-guide-code
 
+-- https://github.com/academind/sql-complete-guide-code/blob/04-data-definition/sql/18-generated-columns.sql
+
 DROP TABLE users;
 DROP TABLE employers;
 DROP TABLE conversations;
 
--- CREATE TYPE employment_status as ENUM('self-employed', 'employed', 'unemployed');
+-- CREATE TYPE employment_status as ENUM('self-employed', 'employed', 'unemployed'); -- Postgresql
 
 CREATE TABLE users(
     -- id SERIAL PRIMARY KEY, --postgresql
-	-- MySQL
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
     first_name VARCHAR(200) NOT NULL,
     last_name VARCHAR(200) NOT NULL,
     full_name VARCHAR(401) GENERATED ALWAYS AS (CONCAT(first_name, ' ', last_name)),
     -- full_name VARCHAR(300) NOT NULL,
-    -- yearly_salary INT CHECK (yearly_salary > 0),
-
+    yearly_salary INT CHECK (yearly_salary > 0),
     current_status ENUM('self-employed', 'employed', 'unemployed') -- MySQL
     -- current_status employment_status
 );
@@ -24,7 +24,7 @@ CREATE TABLE users(
 CREATE TABLE employers(
     -- id SERIAL PRIMARY KEY, -- Postgresql
     -- MySQL
-    id INT PRIMARY KEY AUTO_INCREMENT,     
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL     
     company_name VARCHAR(300) NOT NULL,
     company_address VARCHAR(300) NOT NULL,
     yearly_revenue FLOAT CHECK(yearly_revenue > 0),
@@ -33,9 +33,11 @@ CREATE TABLE employers(
 
 CREATE TABLE conversations(
     -- id SERIAL PRIMARY KEY, --Postgresql
-    -- MySQL
-    id INT PRIMARY KEY AUTO_INCREMENT, 
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL 
     user_id INT,
     employer_id INT,
     message TEXT NOT NULL
 );
+
+INSERT INTO users (first_name, last_name, yearly_salary, current_status)
+VALUES('Max', 'Schwarz', 19999, 'self-employed');
